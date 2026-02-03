667e532251f838069078c14938740cdcb05facc9
Cody Shimizu <chompybird75@gmail.com>
2026-02-02 20:11:39 -0800

    Add files via upload

--- server/spotpass.go
+++ server/spotpass.go
@@ -1,26 +1,38 @@
-// Server code written by OniOkami666
 package main
 
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
-func init_routes(mux *http.ServeMux) {
-	mux.HandleFunc("/CHECK", Check)
-	mux.HandleFunc("/49/", serve_episode)
+func init_routes(mux *http.ServeMux) { // Initialize the routes in the server when it goes online
+	mux.HandleFunc("/1/49/1/CHECK", Check)
+	mux.HandleFunc("/1/49/1/", serve_episode)
 
 }
 
 func Check(w http.ResponseWriter, r *http.Request) { // NV sends a CHECK command to here
 	log.Println("Incoming CHECK request!")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.WriteHeader(http.StatusNotFound) // we want it to return a 404
+}
+func grab_policyfile(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("policylist.xml")
+	if err != nil {
+		log.Printf("Could not read the policy file!  %v", err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(data)
 }
 func serve_episode(w http.ResponseWriter, r *http.Request) {
 	log.Println("Incoming Episode request!", r.URL.Path)
 
-	vids := "../" + r.URL.Path
-	http.ServeFile(w, r, vids)
-}
+	// octet streaming for binary streaming
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 
+	Path := "./ESE_MD1"
+	http.ServeFile(w, r, Path)
+}
