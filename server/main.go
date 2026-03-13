899b5339b651afe5c91784852515d8c3a015644f
Cody Shimizu <chompybird75@gmail.com>
2026-03-13 04:22:00 -0700

    Update main.go

--- server/main.go
+++ server/main.go
@@ -9,9 +9,10 @@ func main() {
 	mux := http.NewServeMux()
 	init_routes(mux)
 
-	log.Println("Running on port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Println("Running on port 80")
+	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
