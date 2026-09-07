bc9adef1c57e4640c587c16a620562f92c31bd69
Cody Shimizu <chompybird75@gmail.com>
2026-09-07 14:03:18 -0700

    Add files via upload

--- server/main.go
+++ server/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Register routes defined in spotpass.go
 	init_routes(mux)
 
-	log.Println("Running on port 80")
-	err := http.ListenAndServe(":80", mux)
+	log.Println("=== Nintendo 3DS/2DS Emulation Server Active ===")
+	log.Println("Listening on http://10.0.0.49:443 ...")
+
+	cert := "mitmproxy-ca.pem"
+	key := "mitmproxy-ca.pem"
+
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS10, // Allows TLS 1.1
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			},
+			GetCertificate: func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				// Log the incoming SNI hostname to see what the 3DS is requesting
+				log.Printf("TLS Handshake requested for SNI: %s", info.ServerName)
+
+				cert, err := tls.LoadX509KeyPair(cert, key)
+				if err != nil {
+					return nil, err
+				}
+				return &cert, nil
+			},
+		},
+	}
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Server crashed: %v", err)
 	}
 }
-
--- server/spotpass.go
+++ server/spotpass.go
@@ -1,39 +1,132 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func init_routes(mux *http.ServeMux) { // Initialize the routes in the server when it goes online
-	mux.HandleFunc("/1/49/1/CHECK", Check)
+func init_routes(mux *http.ServeMux) {
+	mux.HandleFunc("/AC/", handleAutoConnect)
 	mux.HandleFunc("/p01/policylist/3/policylist.xml", grab_policyfile)
-	mux.HandleFunc("/1/49/1/", serve_episode)
-
+	mux.HandleFunc("/p01/recv/", Boss_Recv)
+	mux.HandleFunc("/LogServer_us_live/Upload", handleLogUpload)
+	mux.HandleFunc("/1/49/1/ESE_MD1", serve_episode)
+	mux.HandleFunc("/1/", handleAppRequests)
+	mux.HandleFunc("/", handleUnknown)
 }
+func handleAppRequests(w http.ResponseWriter, r *http.Request) {
+	
+	path := r.URL.Path
+	logRequestDetails("APP-REQ", r)
 
+	switch {
+	case strings.HasSuffix(path, "/CHECK"):
+		Check(w, r)
+	case strings.Contains(path, "ESE_MD"):
+		serve_episode(w, r)
+	default:
+		handleUnknown(w, r)
+	}
+}
+func servePolicyList(w http.ResponseWriter, r *http.Request) {
+    r.Header.Del("Proxy-Authorization")
+    w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    http.ServeFile(w, r, "policylist.xml")
+}
 func Check(w http.ResponseWriter, r *http.Request) { // NV sends a CHECK command to here
 	log.Println("Incoming CHECK request!")
-	w.WriteHeader(http.StatusNotFound) // we want it to return a 404
+
+	body := []byte("OK")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Organization", "Nintendo")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(body)))
+	w.Header().Set("Connection", "close")
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
 }
 func grab_policyfile(w http.ResponseWriter, r *http.Request) {
+	logRequestDetails("POLICY", r)
+
 	data, err := os.ReadFile("policylist.xml")
 	if err != nil {
-		log.Printf("Could not read the policy file!  %v", err)
-		http.Error(w, "Internal Server Error", 500)
+		log.Printf("[POLICY ERROR] Could not read policylist.xml: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-Type", "application/xml")
+
+	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	w.Header().Set("Connection", "close")
+	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 }
 func serve_episode(w http.ResponseWriter, r *http.Request) {
-	log.Println("Incoming Episode request!", r.URL.Path)
+	logRequestDetails("EPISODE", r)
 
-	// octet streaming for binary streaming
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Connection", "close")
 
-	Path := "./ESE_MD1"
-	http.ServeFile(w, r, Path)
+	http.ServeFile(w, r, "./ESE_MD1")
+}
+func handleLogUpload(w http.ResponseWriter, r *http.Request) {
+    logRequestDetails("LOG-UPLOAD", r)
+
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Header().Set("X-Organization", "Nintendo")
+    w.Header().Set("Connection", "close")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+}
+func Boss_Recv(w http.ResponseWriter, r *http.Request) {
+	logRequestDetails("BOSS-RECV", r)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Header().Set("X-Organization", "Nintendo")
+    w.Header().Set("Connection", "close")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+}
+func handleUnknown(w http.ResponseWriter, r *http.Request) {
+    logRequestDetails("CATCH-ALL", r)
+    // Respond 200 OK to CTR Auto-Connect tests hitting "/"
+
+    if r.URL.Path == "/" {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.Header().Set("X-Organization", "Nintendo")
+        w.Header().Set("Connection", "close")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+        return
+    }
+    // Default 404 for unhandled paths
+    w.Header().Set("Content-Type", "text/plain")
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("404 Not Found"))
+}
+
+// Shared helper function to log header details
+func logRequestDetails(tag string, r *http.Request) {
+	log.Printf("[%s] %s %s from %s", tag, r.Method, r.URL.Path, r.RemoteAddr)
+	log.Printf("[%s] User-Agent: %s", tag, r.UserAgent())
+
+	for name, values := range r.Header {
+		for _, value := range values {
+			log.Printf("   -> Header: %s = %s", name, value)
+		}
+	}
+}
+func handleAutoConnect(w http.ResponseWriter, r *http.Request) {
+    logRequestDetails("CTR-AC", r)
+
+    // Standard 3DS response for network checks
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Header().Set("X-Organization", "Nintendo")
+    w.Header().Set("Connection", "close")
+    w.WriteHeader(http.StatusOK)
+
+    w.Write([]byte("OK"))
 }
