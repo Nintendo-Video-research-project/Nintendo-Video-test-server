9ed8b725d193c87166369aa7588dafe90951ada9
Cody Shimizu <chompybird75@gmail.com>
2026-09-08 19:31:01 -0700

    Update main.go

--- server/main.go
+++ server/main.go
@@ -13,7 +13,7 @@ func main() {
 	init_routes(mux)
 
 	log.Println("=== Nintendo 3DS/2DS Emulation Server Active ===")
-	log.Println("Listening on http://10.0.0.49:443 ...")
+	log.Println("Listening on")
 
 	cert := "mitmproxy-ca.pem"
 	key := "mitmproxy-ca.pem"
